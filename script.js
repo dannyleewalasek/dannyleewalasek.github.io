059315eb84e2703aa530f8336f68977d21b61606
dannyleewalasek <dannyleewalasek@gmail.com>
2021-06-25 13:56:13 +0100

    initial commit, moved files from previous test repo

---
+++ script.js
@@ -0,0 +1,80 @@
+const navigationButtons = document.querySelectorAll(".link");
+
+navigationButtons.forEach((link) => {
+  link.addEventListener("click", (el) => {
+    link.classList += " selectedlink";
+    const associatedDiv = document.querySelector(link.id);
+    associatedDiv.scrollIntoView();
+    associatedDiv.classList.remove("scroll");
+    associatedDiv.classList.add("scrolled");
+    navigationButtons.forEach((link2) => {
+      link === link2 ? null : (link2.className = "link");
+    });
+  });
+});
+
+const MakeActive = (event) => {
+  console.log(event);
+};
+
+languageIcons = [
+  "adobe.svg",
+  "es6.svg",
+  "git.svg",
+  "html.svg",
+  "javascript.svg",
+  "node.svg",
+  "npm.svg",
+  "react.svg",
+  "redux.svg",
+  "typescript.svg",
+  "vscode.svg",
+];
+
+let languagesString = "";
+
+languageIcons.forEach(
+  (link) =>
+    (languagesString = languagesString.concat(
+      `<object class="language" data="./prog/svg/${link}" alt="programming language logo"></object>`
+    ))
+);
+
+document
+  .querySelector(".programmingLanguages")
+  .insertAdjacentHTML("afterbegin", languagesString);
+
+const contactIconSVG = [
+  "github.svg",
+  "insta.svg",
+  "linkedin.svg",
+  "twitter.svg",
+  "whatsapp.svg",
+];
+
+contactString = "";
+
+contactIconSVG.forEach(
+  (link) =>
+    (contactString = contactString.concat(
+      `<object type="image/svg+xml" class="contactIcon" data="./contact_svg/${link}" alt="${link.slice(
+        0,
+        link.length - 3
+      )}"></object>`
+    ))
+);
+
+document
+  .querySelector(".footer")
+  .insertAdjacentHTML("afterbegin", contactString);
+
+const contactIcons = document.querySelectorAll(".contactIcon");
+
+document.querySelector(".contactLink").addEventListener("click", () => {
+  contactIcons.forEach((icon) => {
+    icon.className = "contactIcon contactHighlight";
+    setTimeout(function () {
+      icon.className = "contactIcon";
+    }, 2000);
+  });
+});
